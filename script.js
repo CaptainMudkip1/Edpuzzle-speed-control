75c55597274f3a6ba6f294830daca4464fdcc3e1
CaptainMudkip1 <73603426+CaptainMudkip1@users.noreply.github.com>
2023-09-22 22:42:05 -0400

    Inital upload

---
+++ script.js
@@ -0,0 +1,203 @@
+if (!window.href.location.startsWith("https://edpuzzle.com")) {
+  alert("To use this, drag this button into your bookmarks bar. Then, run it when you're on an Edpuzzle assignment.");
+  window.stop();
+}
+var popup = null;
+var base_url;
+if (typeof document.dev_env != "undefined") {
+  base_url = document.dev_env;
+}
+else {
+  //get resources off of github to not inflate the jsdelivr stats
+  base_url = "http://localhost";
+}
+
+function http_get(url, callback, headers=[], method="GET", content=null) {
+  var request = new XMLHttpRequest();
+  request.addEventListener("load", callback);
+  request.open(method, url, true);
+
+  if (window.__EDPUZZLE_DATA__ && window.__EDPUZZLE_DATA__.token) {
+    headers.push(["authorization", window.__EDPUZZLE_DATA__.token]);
+  }
+  for (const header of headers) {
+    request.setRequestHeader(header[0], header[1]);
+  }
+  
+  request.send(content);
+}
+
+function init() {
+  if (window.location.hostname == "edpuzzle.hs.vc") {
+    alert("To use this, drag this button into your bookmarks bar. Then, run it when you're on an Edpuzzle assignment.");
+  }
+  else if ((/https{0,1}:\/\/edpuzzle.com\/assignments\/[a-f0-9]{1,30}\/watch/).test(window.location.href)) {
+    getAssignment();
+  }
+  else if (window.canvasReadyState) {
+    handleCanvasURL();
+  }
+  else if (window.schoologyMoreLess) {
+    handleSchoologyURL();
+  }
+  else {
+    alert("Please run this script on an Edpuzzle assignment. For reference, the URL should look like this:\nhttps://edpuzzle.com/assignments/{ASSIGNMENT_ID}/watch");
+  }
+}
+
+function handleCanvasURL() {
+  let location_split = window.location.href.split("/");
+  let url = `/api/v1/courses/${location_split[4]}/assignments/${location_split[6]}`;
+  http_get(url, function(){
+    let data = JSON.parse(this.responseText);
+    let url2 = data.url;
+
+    http_get(url2, function() {
+      let data = JSON.parse(this.responseText);
+      let url3 = data.url;
+
+      alert(`Please re-run this script in the newly opened tab. If nothing happens, then allow popups on Canvas and try again.`);
+      open(url3);
+    });
+  });
+}
+
+function handleSchoologyURL() {
+  let assignment_id = window.location.href.split("/")[4];
+  let url = `/external_tool/${assignment_id}/launch/iframe`;
+  http_get(url, function() {
+    alert(`Please re-run this script in the newly opened tab. If nothing happens, then allow popups on Schoology and try again.`);
+
+    //strip js tags from response and add to dom
+    let html = this.responseText.replace(/<script[\s\S]+?<\/script>/, ""); 
+    let div = document.createElement("div");
+    div.innerHTML = html;
+    let form = div.querySelector("form");
+    
+    let input = document.createElement("input")
+    input.setAttribute("type", "hidden");
+    input.setAttribute("name", "ext_submit");
+    input.setAttribute("value", "Submit");
+    form.append(input);
+    document.body.append(div);
+
+    //submit form in new tab
+    form.setAttribute("target", "_blank");
+    form.submit();
+    div.remove();
+  });
+}
+
+function getAssignment(callback) {
+  var assignment_id = window.location.href.split("/")[4];
+  if (typeof assignment_id == "undefined") {
+    alert("Error: Could not infer the assignment ID. Are you on the correct URL?");
+    return;
+  }
+  var url1 = "https://edpuzzle.com/api/v3/assignments/"+assignment_id;
+
+  http_get(url1, function(){
+    var assignment = JSON.parse(this.responseText);
+    if ((""+this.status)[0] == "2") {
+      openPopup(assignment);
+    }
+    else {
+      alert(`Error: Status code ${this.status} recieved when attempting to fetch the assignment data.`)
+    }
+  });
+}
+
+function openPopup(assignment) {
+  var media = assignment.medias[0];
+  var teacher_assignment = assignment.teacherAssignments[0];
+  var assigned_date = new Date(teacher_assignment.preferences.startDate);
+  var date = new Date(media.createdAt);
+  thumbnail = media.thumbnailURL;
+  if (thumbnail.startsWith("/")) {
+    thumbnail = "https://"+window.location.hostname+thumbnail;
+  }
+  
+  var deadline_text;
+  if (teacher_assignment.preferences.dueDate == "") {
+    deadline_text = "no due date"
+  }
+  else {
+    deadline_text = "due on "+(new Date(teacher_assignment.preferences.dueDate)).toDateString();
+  }
+  
+  var base_html = `
+  <!DOCTYPE html>
+  <head>
+    <style>
+      * {font-family: Arial}
+    </style>
+    <script>
+      var base_url = "${base_url}";
+      function http_get(url, callback) {
+        var request = new XMLHttpRequest();
+        request.addEventListener("load", callback);
+        request.open("GET", url, true);
+        request.send();
+      }
+      function get_tag(tag, url) {
+        console.log("Loading "+url);
+        http_get(url, function(){
+          if ((""+this.status)[0] == "2") {
+            var element = document.createElement(tag);
+            element.innerHTML = this.responseText;
+            document.getElementsByTagName("head")[0].appendChild(element);
+          }
+          else {
+            console.error("Could not fetch "+url);
+          }
+        });
+      }
+      get_tag("style", base_url+"/app/popup.css");
+      get_tag("script", base_url+"/app/videooptions.js");
+      get_tag("script", base_url+"/app/videospeed.js");
+    </script>
+    <title>Video settings for: ${media.title}</title>
+  </head>
+  <div id="header_div">
+    <div>
+      <img src="${thumbnail}" height="108px">
+    </div>
+    <div id="title_div">
+      <p style="font-size: 16px"><b>${media.title}</b></h2>
+      <p style="font-size: 12px">Uploaded by ${media.user.name} on ${date.toDateString()}</p>
+      <p style="font-size: 12px">Assigned on ${assigned_date.toDateString()}, ${deadline_text}</p>
+      <div id="speed_container" hidden>
+        <label style="font-size: 12px" for="speed_dropdown">Video speed:</label>
+        <select name="speed_dropdown" id="speed_dropdown" onchange="video_speed()">
+          <option value="0.25">0.25</option>
+          <option value="0.5">0.5</option>
+          <option value="0.75">0.75</option>
+          <option value="1" selected>Normal</option>
+          <option value="1.25">1.25</option>
+          <option value="1.5">1.5</option>
+          <option value="1.75">1.75</option>
+          <option value="2">2</option>
+          <option value="-1">Custom</option>
+        </select>
+        <label id="custom_speed_label" style="font-size: 12px" for="custom_speed"></label>
+        <input type="range" id="custom_speed" name="custom_speed" value="1" min="0.1" max="16" step="0.1" oninput="video_speed()" hidden>
+      </div>
+      <div id="options_container">
+        <label for="pause_on_focus" style="font-size: 12px">Don't pause on unfocus: </label>
+        <input type="checkbox" id="pause_on_focus" name="pause_on_focus" onchange="toggle_unfocus();">
+      </div>
+    </div>
+  </div>
+  <div id="content"> 
+    <p style="font-size: 12px" id="loading_text"></p>
+  </div>`;
+  popup = window.open("about:blank", "", "width=600, height=225");
+  popup.document.write(base_html);
+
+  popup.document.assignment = assignment;
+  popup.document.dev_env = document.dev_env;
+  popup.document.edpuzzle_data = window.__EDPUZZLE_DATA__;
+
+}
+
+init();
